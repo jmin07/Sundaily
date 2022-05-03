5618ea6e1405464b28b9fb99c0dac09dfa8eea81
Jong Min <guentc1@naver.com>
2022-05-03 11:08:28 +0900

    0503

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>sunday billy</h1>
+    <div className="grid-container">
+      <div className='sidebar-a grid-item'>Sidebar A</div>
+      <div className='sidebar-b grid-item'>Sidebar B</div>
+      <div className='main grid-item'>Main</div>
     </div>
   );
 }
